dd0e5a81f932bfceed9cc3e601ae9c4ea04d3574
Andrew Edson <C_Edson.Andrew@cerner.com>
2019-11-25 09:19:53 -0600

    Adds user serializer for customizing the json output

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,32 +2,29 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[show update destroy]
   before_action :check_owner, only: %i[update destroy]
 
-  # GET /users/1
   def show
-    render json: @user
+    options = { include: [:products] }
+    render json: UserSerializer.new(@user, options).serializable_hash
   end
 
-  # POST /users
+  def update
+    if @user.update(user_params)
+      render json: UserSerializer.new(@user).serializable_hash
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: UserSerializer.new(@user).serializable_hash, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user, status: :ok
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /users/1
   def destroy
     @user.destroy
     head 204
@@ -35,16 +32,16 @@ class Api::V1::UsersController < ApplicationController
 
   private
 
-  # Only allow a trusted parameter "white list" through
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
+  end
+
+  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:email, :password)
   end
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def check_owner
-    head :forbidden unless @user.id == current_user&.id
   end
 end
